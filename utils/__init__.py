28591e77cb4e56387397201bcf6aa42d90feb3db
markadc <markadc@126.com>
2025-02-19 15:31:44 +0800

    log可以指定日志等级

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 from cocoman.utils.decor import *
 from cocoman.utils.func import *
-from cocoman.utils.loger import *
-from cocoman.utils.pool import *
-from cocoman.utils.ua import *
+from cocoman.utils.log import Log
+from cocoman.utils.pool import PoolWait, PoolMan
+from cocoman.utils.proxy_api import ProxyAPI
+from cocoman.utils.ua import gen_random_ua
